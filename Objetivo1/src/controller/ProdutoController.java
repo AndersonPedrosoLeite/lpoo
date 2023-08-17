075948c242c7c5e3d9d69d6e216eab942102a628
andersonpleite <andersonpleite2022@gmail.com>
2023-08-17 20:25:06 -0300

    exercícios para desenvolvimento do objetivo1

--- Objetivo1/src/controller/ProdutoController.java
+++ Objetivo1/src/controller/ProdutoController.java
@@ -2,7 +2,7 @@ package controller;
 import model.Produto;
 public class ProdutoController {
     public static void main(String[] args){
-        Produto produto1 = new Produto("Correa", "Correa dentada do motor", 228.75, 43);
+        Produto produto1 = new Produto("Correia", "Correia dentada do motor", 228.75, 43);
         System.out.println(produto1);
     }
 }
