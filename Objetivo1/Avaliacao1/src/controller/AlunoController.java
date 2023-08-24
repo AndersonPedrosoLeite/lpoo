e256d52d9b32c1b27e2e623a275bc73e3a64c0a5
andersonpleite <andersonpleite2022@gmail.com>
2023-08-24 16:19:56 -0300

    Avaliação 1

--- Objetivo1/Avaliacao1/src/controller/AlunoController.java
+++ Objetivo1/Avaliacao1/src/controller/AlunoController.java
@@ -4,6 +4,9 @@ import model.Aluno;
 
 public class AlunoController {
     public static void main(String[] args) {
-        Aluno aluno1 = new Aluno();
+        Aluno aluno1 = new Aluno(1,377255,"João","Leivas","joao@gmail.com");
+        Aluno aluno2 = new Aluno(2,484837,"Maria","Amaral","maria@gmail.com");
+        Aluno aluno3 = new Aluno(3,9224444,"Joice","Brusque","joice@gmail.com");
+        Aluno aluno4 = new Aluno(4,298440,"Cristina","Ruchell","cris@gmail.com");
     }
 }
