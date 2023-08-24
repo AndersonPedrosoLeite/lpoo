3707b666b5f72f18f95f2d56aa6970426a58d1fa
andersonpleite <andersonpleite2022@gmail.com>
2023-08-23 21:28:43 -0300

    Criação dos métodos da classe Conta

--- Objetivo1/src/controller/ContaController.java
+++ Objetivo1/src/controller/ContaController.java
@@ -1,7 +1,58 @@
 package controller;
 
+import model.Carro;
+import model.Conta;
+
+import java.util.*;
+
 public class ContaController {
     public static void main(String[] args){
+        Conta conta1 = new Conta(1,23000.00);
+        Conta conta2 = new Conta(2,20000.30);
+        Conta conta3 = new Conta(3,3200.75);
+        Conta conta4 = new Conta(4,7300.50);
+        Conta conta5 = new Conta(5,5500.00);
 
+        System.out.println(conta1);
+        System.out.println(conta2);
+        System.out.println(conta3);
+        System.out.println(conta4);
+        System.out.println(conta5);
+
+        conta3.deposita(10000.00);
+
+        System.out.println("Saldo da conta "+ conta3.getId() + " =R$ " + conta3.getSaldo());
+
+        conta5.saca(3000.00);
+        conta5.atualizar(25);
+        System.out.println("Taxa aplicada com sucesso, seu saldo foi atualizado!");
+        System.out.println("O novo saldo da conta "+ conta5.getId() +  " é =R$ " + conta5.getSaldo());
+
+        List<Conta> listaContas = new ArrayList<>();
+        listaContas.add(conta1);
+        listaContas.add(conta2);
+        listaContas.add(conta3);
+        listaContas.add(conta4);
+        listaContas.add(conta5);
+
+
+        listaContas.sort(Comparator.comparing(Conta::getSaldo).reversed());
+        System.out.println("\nOrdenando as contas pelo saldo");
+        System.out.println(listaContas);
+
+        System.out.println("\n Estes são os saldos de todas as contas: \n" + listaContas);
+        Conta contaBusca = listaContas.stream().filter(c -> c.getId() == 2).findAny().orElse(null);
+        System.out.println(contaBusca);
+
+        Map<Integer, Conta> contasMap = new HashMap<>();
+        contasMap.put(conta1.getId(), conta1);
+        contasMap.put(conta2.getId(), conta2);
+        contasMap.put(conta3.getId(), conta3);
+        contasMap.put(conta4.getId(), conta4);
+        contasMap.put(conta5.getId(), conta5);
+
+        System.out.println("\nBusca pelas conta de id=1 e id=4 na coleção Map");
+        System.out.println(contasMap.get(1));
+        System.out.println(contasMap.get(4));
     }
 }
--- Objetivo1/src/model/Conta.java
+++ Objetivo1/src/model/Conta.java
@@ -1,13 +1,52 @@
 package model;
 
 public class Conta {
+    private Integer id;
     private Double saldo;
 
     public Conta() {
     }
 
-    public Conta(Double saldo) {
+    public Conta(Integer id, Double saldo) {
+        this.id = id;
         this.saldo = saldo;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+    public void deposita(Double saldo){
+        this.saldo = saldo;
+    }
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void saca(Double valor){
+        Double valorTemporario = this.saldo - valor;
+        if(valorTemporario < 0 ){
+            System.out.println("Saldo insuficiente. Saldo= " + this.saldo);
+            return;
+        }
+        this.saldo -= valor;
+        System.out.println("\nSaque realizado com sucesso. Saldo atual = " + this.saldo);
+    }
+    public void atualizar(double taxa) {
+        this.saldo += this.saldo * (taxa/100);
+       // System.out.println("Saldo atualizado! Saldo atual=" + this.saldo);
+    }
+
+    @Override
+    public String toString() {
+        return "\nConta{" + "id=" + id + ", saldo=" + saldo + '}' ;
+    }
 }
