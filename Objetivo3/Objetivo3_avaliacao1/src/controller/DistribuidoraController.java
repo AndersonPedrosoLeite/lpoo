07165861246b0b1df3a8e9af99ff2850bf60d973
Anderson Leite <andersonleite.pl013@academico.ifsul.edu.br>
2023-10-24 22:05:38 -0300

    Continuação da avaliação 3

--- Objetivo3/Objetivo3_avaliacao1/src/controller/DistribuidoraController.java
+++ Objetivo3/Objetivo3_avaliacao1/src/controller/DistribuidoraController.java
@@ -1,12 +1,27 @@
 package controller;
 
-import model.Produto;
+import model.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DistribuidoraController {
     public static void main(String[] args) {
-        Produto teclado = new Produto(1,"teclado",300,223.50,"periférico");
-        Produto monitor = new Produto(2,"monitor",50,799.80,"hardware");
-        Produto mouse = new Produto(3,"mouse",74,89.90,"periférico");
-        Produto processador = new Produto(4,"processador Ryzen 7",138,1134.50,"hardware");
+        Produto teclado = new Produto(1, "teclado", 300, 223.50, "periférico");
+        Produto monitor = new Produto(2, "monitor", 50, 799.80, "hardware");
+        Produto mouse = new Produto(3, "mouse", 74, 89.90, "periférico");
+        Produto processador = new Produto(4, "processador Ryzen 7", 138, 1134.50, "hardware");
+        Tipo tipo1 = new Tipo(1,"fortrek",new Pedido());
+        Item item1 = new Item(1,4.5,4,teclado);
+
+        List<Pedido> pedidosList = null;
+        Vendedor vendedor1 = new Vendedor(23, "Marisa", "Rua Tiradentes", "Centro", "29839823", "Pelotas", "RS", "Padaria", pedidosList);
+        Pedido pedido1 = new Pedido(1, "25/10/2023", 223.5,vendedor1,tipo1,item1);
+
+        pedidosList = new ArrayList<>();
+        pedidosList.add(pedido1);
+
+        System.out.println(pedidosList);
+
     }
 }
--- Objetivo3/Objetivo3_avaliacao1/src/model/Item.java
+++ Objetivo3/Objetivo3_avaliacao1/src/model/Item.java
@@ -6,4 +6,56 @@ public class Item {
     private Integer quantidade;
     // um item compõe um pedido//
     Produto produto;
+
+    public Item() {
+    }
+
+    public Item(int codItem, double desconto, Integer quantidade, Produto produto) {
+        this.codItem = codItem;
+        this.desconto = desconto;
+        this.quantidade = quantidade;
+        this.produto = produto;
+    }
+
+    public int getCodItem() {
+        return codItem;
+    }
+
+    public void setCodItem(int codItem) {
+        this.codItem = codItem;
+    }
+
+    public double getDesconto() {
+        return desconto;
+    }
+
+    public void setDesconto(double desconto) {
+        this.desconto = desconto;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+    @Override
+    public String toString() {
+        return "\nItem{" +
+                "codItem=" + codItem +
+                ", desconto=" + desconto +
+                ", quantidade=" + quantidade +
+                ", produto=" + produto +
+                '}';
+    }
 }
